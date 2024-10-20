2e721295cd9fec388402fe06b85f069fa450160b
Parker McCrary <parker.mccrary@journeygroup.com>
2024-10-20 12:38:36 -0400

    build: add initial modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/scripts',
+    '@vueuse/nuxt'
+  ]
 })
